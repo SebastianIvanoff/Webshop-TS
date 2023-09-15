946ab95eb17a0a5c0581a6f0483206f4884ded63
SebastianIvanoff <sebbe.ivanoff@gmail.com>
2023-09-15 10:47:34 +0200

    img fix

--- Webshop-TS/src/Types.tsx
+++ Webshop-TS/src/Types.tsx
@@ -12,7 +12,7 @@ const products: Product[] = [
     name: "Apple",
     price: 10,
     imgURL:
-      "https://img.freepik.com/premium-psd/ripe-whole-green-apple-with-half-leaf_34168-1625.jpg?w=1380",
+      "https://img.freepik.com/free-photo/green-apple-with-leaves_1101-453.jpg?w=740&t=st=1694767394~exp=1694767994~hmac=35cc27ac88cc21d04659eadf4e23ac120c7f8801200a83ca7557eb466367c923",
     description: "Juicy apple",
   },
   {
@@ -20,7 +20,7 @@ const products: Product[] = [
     name: "Orange",
     price: 12,
     imgURL:
-      "https://img.freepik.com/premium-psd/fresh-orange-sliced-with-leaves-isolated-premium-psd_328278-1570.jpg?w=996",
+      "https://img.freepik.com/free-photo/orange-white-white_144627-16571.jpg?w=740&t=st=1694767415~exp=1694768015~hmac=731c2f9cb8e813514f9d30e90e31646854b20139b8a6009445f98db984c32c54",
     description: "Tasty Orange",
   },
   {
@@ -28,7 +28,7 @@ const products: Product[] = [
     name: "Banana",
     price: 12,
     imgURL:
-      "https://img.freepik.com/premium-psd/banana-isolated_88281-5284.jpg",
+      "https://img.freepik.com/free-vector/vector-ripe-yellow-banana-bunch-isolated-white-background_1284-45456.jpg?w=740&t=st=1694767461~exp=1694768061~hmac=db51fc76d17ebcc686deeb27681c83ac74f6b0738b3a2a9ab2b9b032f2c7662c",
     description: "Tasty Banana",
   },
   {
@@ -36,7 +36,7 @@ const products: Product[] = [
     name: "Strawberry",
     price: 12,
     imgURL:
-      "https://img.freepik.com/premium-psd/strawberries-isolated_328278-300.jpg",
+      "https://img.freepik.com/free-photo/closeup-shot-fresh-ripe-strawberries-isolated-white-surface_181624-54939.jpg?w=740&t=st=1694767486~exp=1694768086~hmac=60560355a66bd5d4abe046c1e32dae1a91456882be94e9f34367c1e38bce1ae2",
     description: "Tasty Strawberry",
   },
   {
@@ -44,7 +44,7 @@ const products: Product[] = [
     name: "Pineapple",
     price: 12,
     imgURL:
-      "https://img.freepik.com/premium-psd/fresh-pineapple-isolated-premium-psd_328278-735.jpg",
+      "https://img.freepik.com/free-photo/pineapple-juicy-mellow-isolated-white_179666-678.jpg?w=826&t=st=1694767509~exp=1694768109~hmac=d414b994ebd7303bae38203993c968995f20b19b577f192e89527cef9714e36e",
     description: "Tasty Pineapple",
   },
   {
@@ -52,7 +52,7 @@ const products: Product[] = [
     name: "Grapes",
     price: 12,
     imgURL:
-      "https://img.freepik.com/premium-psd/fresh-red-grape-with-leaves-isolated-premium-psd_328278-798.jpg?w=1060",
+      "https://img.freepik.com/free-photo/fresh-red-grapes-with-leaves-isolated-white_167946-37.jpg?w=826&t=st=1694767528~exp=1694768128~hmac=23fe46aa7082a10d0ad6b301ee57d753d189826977aecc343e905acd5d9f27d2",
     description: "Tasty Grapes",
   },
   {
@@ -60,7 +60,7 @@ const products: Product[] = [
     name: "Watermelon",
     price: 12,
     imgURL:
-      "https://img.freepik.com/premium-psd/watermelon-isolated-premium-psd_328278-538.jpg",
+      "https://img.freepik.com/free-photo/green-striped-ripe-watermelon-with-slice-cross-section-isolated-white-background-with-copy-space-text-images-special-kind-berry-sweet-pink-flesh-with-black-seeds-side-view_639032-1254.jpg?w=740&t=st=1694767546~exp=1694768146~hmac=a0ebfbb54c9de47e14a2a5988aa2f69bb95e93d1788839935375f682928e05a1",
     description: "Tasty Watermelon",
   },
   {
@@ -68,7 +68,7 @@ const products: Product[] = [
     name: "Kiwi",
     price: 12,
     imgURL:
-      "https://img.freepik.com/premium-psd/fresh-kiwi-fruit-with-leaves-isolated-premium-psd_328278-840.jpg",
+      "https://img.freepik.com/free-photo/fresh-kiwi-fruit-isolated_144627-30034.jpg?w=740&t=st=1694767565~exp=1694768165~hmac=39f8dbd95893c9ac906dfda3f0dadfcb6ff7f634b491cdbce669860a3d381d11",
     description: "Tasty Kiwi",
   },
   {
@@ -76,7 +76,7 @@ const products: Product[] = [
     name: "Blueberry",
     price: 12,
     imgURL:
-      "https://img.freepik.com/premium-psd/fresh-blueberry-with-leaves-isolated-premium-psd_328278-945.jpg?w=1380",
+      "https://img.freepik.com/free-photo/juniper-berries-levitate-white-background_485709-58.jpg?w=740&t=st=1694767595~exp=1694768195~hmac=353f4398c512494828ab7a613d856446e2fe17253b103d323207d24dec21adfc",
     description: "Tasty Blueberry",
   },
   {
@@ -84,7 +84,7 @@ const products: Product[] = [
     name: "Mango",
     price: 12,
     imgURL:
-      "https://img.freepik.com/premium-psd/sliced-mango-alpha-layer_610539-436.jpg?w=1800",
+      "https://img.freepik.com/free-photo/mango-table_144627-16763.jpg?w=740&t=st=1694767612~exp=1694768212~hmac=c32fad83d6e1e3bf826376de1d63d379a5f68bb2a92725e017f893f44f848f84",
     description: "Tasty Mango",
   },
 ];
